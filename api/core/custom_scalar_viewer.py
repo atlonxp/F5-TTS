06347bf41d3349fc1032b6a0a48158151e679b4c
atlonxp <atlonxp@me.com>
2025-03-10 06:40:58 +0700

    revised scalar

--- api/core/custom_scalar_viewer.py
+++ api/core/custom_scalar_viewer.py
@@ -203,7 +203,7 @@ def get_custom_scalar_api_reference(
 class CustomScalarViewer(ScalarViewer):
     def __init__(
             self,
-            title: str = "API Reference",
+            title: str = "API Reference | F5-TTS by Watthanasak J.",
             openapi_url: str = "/api/openapi.json",
             scalar_js_url: str = "https://cdn.jsdelivr.net/npm/@scalar/api-reference",  # noqa: E501
             scalar_proxy_url: str = "",
